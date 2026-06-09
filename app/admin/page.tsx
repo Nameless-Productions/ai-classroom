c937879d5d882a173a92f68a0f1c82fcb4fa3393
thenamelessdev <nameless@thenamelessdev.com>
2026-06-09 13:43:41 +0200

    feat: add class list UI

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,3 +1,4 @@
+import { getClasses } from '@/lib/classes/getClasses'
 import { getUsers } from '@/lib/users/getUsers'
 import React from 'react'
 
@@ -5,8 +6,19 @@ export default async function AdminPage() {
     const users = await (await getUsers()).map((u) => (
         <li key={u.id}>ID: {u.id} <br />Username: {u.username} <br />Role: {u.role}</li>
     ))
+
+    const classes = await (await getClasses()).map((c) => (
+      <li key={c.id}>ID: {c.id} <br />Name: {c.name}</li>
+    ))
   return (<>
     <p className='font-bold text-xl'>Info</p>
+    <br />
+
+    <p className='font-bold'>All classes:</p>
+    <ul className='list-inside ml-2 space-y-2'>
+      {classes}
+    </ul>
+
     <br />
     <p className='font-bold'>All users:</p>
     <ul className='list-inside ml-2 space-y-4'>
