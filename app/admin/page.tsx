9442fb6380ae698820bd960d750cae7a6c069c2f
thenamelessdev <nameless@thenamelessdev.com>
2026-06-09 11:52:19 +0200

    feat: add users list to info page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,16 @@
+import { getUsers } from '@/lib/users/getUsers'
 import React from 'react'
 
-export default function AdminPage() {
+export default async function AdminPage() {
+    const users = await (await getUsers()).map((u) => (
+        <li key={u.id}>ID: {u.id} <br />Username: {u.username} <br />Role: {u.role}</li>
+    ))
   return (<>
-    <p className='font-bold'>Coming soon!</p>
+    <p className='font-bold text-xl'>Info</p>
+    <br />
+    <p className='font-bold'>All users:</p>
+    <ul className='list-inside ml-2 space-y-4'>
+        {users}
+    </ul>
   </>)
 }
