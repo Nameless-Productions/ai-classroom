a9843243bc53674a13bbd1e41cb9f25803ba72f4
thenamelessdev <nameless@thenamelessdev.com>
2026-06-09 11:46:14 +0200

    feat: add getUsers function

---
+++ lib/users/getUsers.ts
@@ -0,0 +1,19 @@
+"use server";
+
+import { db } from "../db";
+
+export async function getUsers(){
+    const users = await db.users.findMany({
+        select: {
+            id: true,
+            username: true,
+            role: true,
+            password: false
+        },
+        orderBy: {
+            id: "desc"
+        }
+    })
+
+    return users
+}
