e43810804fdcba321beb4b89af1f906c35858300
thenamelessdev <nameless@thenamelessdev.com>
2026-06-09 11:59:39 +0200

    feat: add create class UI

--- app/admin/settings/page.tsx
+++ app/admin/settings/page.tsx
@@ -1,3 +1,4 @@
+import { createClassForm } from '@/lib/classes/createClass'
 import { createUserForm } from '@/lib/users/createUser'
 import React from 'react'
 
@@ -16,6 +17,16 @@ export default function SettingsPage() {
         <input type="password" name='password' className='inp-normal mb-2 mt-2' placeholder='Password' required />
         <br />
         <input type="submit" className='btn-normal w-full' value="Submit" />
+    </form>
+
+    <br />
+
+    <form action={createClassForm}>
+      <p className='font-bold text-lg mb-2'>Create Class</p>
+
+      <input type="text" name='name' placeholder='Class name' className='inp-normal mb-2' required />
+      <br />
+      <input type="submit" value="Create class" className='btn-normal w-full' />
     </form>
   </>)
 }
