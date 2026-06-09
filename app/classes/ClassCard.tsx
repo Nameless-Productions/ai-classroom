9c8bb43e65e85b44c5e691a4ceee393c7b092329
thenamelessdev <nameless@thenamelessdev.com>
2026-06-09 18:34:49 +0200

    update: make class cards the same size

--- app/classes/ClassCard.tsx
+++ app/classes/ClassCard.tsx
@@ -4,7 +4,7 @@ import { redirect } from 'next/navigation'
 import React from 'react'
 
 export default function ClassCard({name, color, id}: {name: string, color: string, id: number}) {
-  return (<div className={`p-10 cursor-pointer text-black rounded-xl ${color}`} onClick={() => redirect(`/classes/${id}`)}>
+  return (<div className={`w-40 h-30 p-2 cursor-pointer text-black rounded-xl ${color}`} onClick={() => redirect(`/classes/${id}`)}>
     <p>{name}</p>
   </div>)
 }
